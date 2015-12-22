ede0ba97601b9272fc5ecaf9eff8c50b6389141d
WillahScott <willie9monge@gmail.com>
2015-12-22 15:54:55 +0100

    debugging x_act VI

--- backend/src/x_act.py
+++ backend/src/x_act.py
@@ -76,10 +76,69 @@ def parse_timestamp(data):
 def parse_date(stmp):
 	''' Convert timestamp into date with HBase row format
 	'''
-	print '--------------------------- PARSING DATES --------------------------------------'
+	print '-------------------------- PARSING DATES --------------------------'
 	_date = datetime.fromtimestamp(float(stmp))
 	date = '-'.join(map(str, [_date.year, _date.month, _date.day]))
 	return date
+
+
+def update_hbase(data):
+	''' Updates the HBase table with given: data = ( <date>, ( <message_count>, <user_count> ) )
+	'''
+
+	# # Parse data
+	# date_str = data[0]
+	# message_count = data[1][0]
+	# act_user_count = data[1][0]
+	date_str = '2015-12-16'
+	message_count = '15'
+	act_user_count = '4'
+
+	# Read actual table
+	hbase_rdd = sc.newAPIHadoopRDD(
+	    "org.apache.hadoop.hbase.mapreduce.TableInputFormat",
+	    "org.apache.hadoop.hbase.io.ImmutableBytesWritable",
+	    "org.apache.hadoop.hbase.client.Result",
+	    keyConverter=keyConv_read,
+	    valueConverter=valueConv_read,
+	    conf=conf_read)
+	hbase_rdd = hbase_rdd.flatMapValues(lambda v: v.split("\n")).mapValues(json.loads)
+	output = hbase_rdd.collect()
+
+
+	# Record values of row corresponding to date
+	update_values = {}
+	for (d, v) in output:
+		if d == date_str:
+			update_values[v['qualifier']] = v['value']
+
+	print "===========> READING HBASE"
+	print "===> Values Read:", update_values
+
+	# # If necessary update values
+	# if len(update_values) > 0:
+	# 	message_count = str( int(update_values['totalMsgs']) + int(message_count) )
+	# 	act_user_count = str( int(update_values['uniqueUsers']) + int(act_user_count) )
+
+
+	# Update the HBase table
+	row1 = ( date_str, [date_str, family, 'totalMsgs', message_count] )
+	row2 = ( date_str, [date_str, family, 'uniqueUsers', act_user_count] )
+	row3 = ( date_str, [date_str, family, 'latestTimestamp', time_latest] )
+
+	print "===========> WRITING HBASE"
+	print "===> Values to write (I/III):", row1
+	print "===> Values to write (II/III):", row2
+	print "===> Values to write (IIII/III):", row3
+
+
+	sc.parallelize([ row1, row2, row3 ]).saveAsNewAPIHadoopDataset(
+	               keyConverter=keyConv_write,
+	               valueConverter=valueConv_write,
+	               conf=conf_write
+	               )
+
+	return True
 
 
 
@@ -95,11 +154,40 @@ def parse_date(stmp):
 
 
 
+## HBASE CONFIGURATION --------------------------------------------------------
+
+# Global host-table configuration
+host = 'localhost:2181'
+table = 'slafka_daily'
+
+
+# Configuration for HBase read
+conf_read = {"hbase.zookeeper.quorum": host, "hbase.mapreduce.inputtable": table}
+keyConv_read = "org.apache.spark.examples.pythonconverters.ImmutableBytesWritableToStringConverter"
+valueConv_read = "org.apache.spark.examples.pythonconverters.HBaseResultToStringConverter"
+
+
+# Configuration for HBase write
+family = 'tsa'
+conf_write = {"hbase.zookeeper.quorum": host,
+              "hbase.mapred.outputtable": table,
+              "mapreduce.outputformat.class": "org.apache.hadoop.hbase.mapreduce.TableOutputFormat",
+              "mapreduce.job.output.key.class": "org.apache.hadoop.hbase.io.ImmutableBytesWritable",
+              "mapreduce.job.output.value.class": "org.apache.hadoop.io.Writable"
+              }
+keyConv_write = "org.apache.spark.examples.pythonconverters.StringToImmutableBytesWritableConverter"
+valueConv_write = "org.apache.spark.examples.pythonconverters.StringListToPutConverter"
+
+
+
+
+
+
 ## STREAM ANALYSIS ------------------------------------------------------------
 
 # Initialize stream
 sc = SparkContext("local[2]", "MyApp")
-ssc = StreamingContext(sc, 10)
+ssc = StreamingContext(sc, 15)
 ssc.checkpoint("file:///apps/new-slafka/slafka-mockup/backend/data/activity/checkpointing")
 
 # Get stream of raw messages from Kafka
@@ -118,18 +206,17 @@ times = raw_msgs.map( parse_timestamp )
    # using windows of 10 minutes, with 1 minute batches
 _message_count = users.count() # 600, 60
 _act_user_count = users.countByValue()
-
-print 'XXXXXXXXX =================================================='
-
 _time_latest = times.reduce( max ).map( parse_date )
 
-
-print 'YYYYYYYYY -------====================================-------'
 
 _counts = _message_count.union(_act_user_count)
 final_stream = _time_latest.union(_counts)
 
+# Debug
 _message_count.pprint()
+
+# Update HBase with each entry
+hbase_updates = _message_count.flatMap( update_hbase )
 
 
 # Collect results
@@ -144,8 +231,6 @@ _message_count.pprint()
 # message_count.pprint()
 # act_user_count.pprint()
 # time_latest.pprint()
-
-
 
 
 
